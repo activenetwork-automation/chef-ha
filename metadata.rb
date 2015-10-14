8025e91a4de2eb32016cd6b1c85f538f1edba6a4
Joe Nguyen <joe.nguyen@activenetwork.com>
2015-10-14 10:28:42 -0500

    description/fc updates

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,14 @@
 name             'chef-ha'
 maintainer       'Active Network'
 maintainer_email 'joe.nguyen@activenetwork.com'
-license          'All rights reserved'
-description      'Installs/Configures chef ha'
-long_description 'Installs/Configures chef ha'
+license          'Apache 2.0'
+description      'Installs/Configures Chef in high availibility'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'nfs', '~> 2.2.4'
+
+supports 'redhat'
+
+source_url 'https://github.com/activenetwork-automation/chef-ha'
+issues_url 'https://github.com/activenetwork-automation/chef-ha/issues'
